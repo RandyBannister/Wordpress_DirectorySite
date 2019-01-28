83dc7145feca9450fea26c4cccd57edd3c947db3
MobileStar <mobilestar@MobileStars-MacBook-Pro.local>
2019-01-27 19:06:25 -0600

    1.0

--- wp-activate.php
+++ wp-activate.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Confirms that the activation key that is sent in an email after a user signs
- * up for a new site matches the key for that user and then displays confirmation.
- *
- * @package WordPress
- */
 
 define( 'WP_INSTALLING', true );
 
--- wp-blog-header.php
+++ wp-blog-header.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Loads the WordPress environment and template.
- *
- * @package WordPress
- */
 
 if ( !isset($wp_did_header) ) {
 
